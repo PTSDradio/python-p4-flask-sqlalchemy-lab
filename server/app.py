23910d47d01f4ce87e95bc88faa25b81bdb295c6
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-08-19 11:42:04 -0500

    completes the coolest lab so far

--- server/app.py
+++ server/app.py
@@ -19,16 +19,45 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    '''
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    keeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    response_body = f'''
+        <ul>ID: {keeper.id}</ul>
+        <ul>Name: {keeper.name}</ul>
+        <ul>Birthday: {keeper.birthday}</ul>
+    '''
+    for animal in keeper.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+    response = make_response(response_body, 200)
+    return response
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
 
+    response_body = f'''
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: { enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+    '''
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+    response = make_response(response_body, 200)
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
